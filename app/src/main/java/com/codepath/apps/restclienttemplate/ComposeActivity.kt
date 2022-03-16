49fe98cb20a774cda6587f61270c03b67513ed37
Timothy Yoon <timsyoon@gmail.com>
2022-03-16 16:21:53 -0700

    Display character count while composing tweet
    Note: Some non-alphabetical characters seem to reset the count.

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
@@ -1,11 +1,15 @@
 package com.codepath.apps.restclienttemplate
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.codepath.apps.restclienttemplate.models.Tweet
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
@@ -15,6 +19,9 @@ class ComposeActivity : AppCompatActivity() {
 
     lateinit var etCompose: EditText
     lateinit var btnTweet: Button
+    lateinit var tvCharacterCount: TextView
+
+    var charactersRemaining = CHARACTER_LIMIT
 
     lateinit var client: TwitterClient
 
@@ -24,6 +31,9 @@ class ComposeActivity : AppCompatActivity() {
 
         etCompose = findViewById(R.id.etTweetCompose)
         btnTweet = findViewById(R.id.btnTweet)
+        tvCharacterCount = findViewById(R.id.tvCharacterCount)
+        tvCharacterCount.text = charactersRemaining.toString()
+        tvCharacterCount.setTextColor(Color.GRAY)
 
         client = TwitterApplication.getRestClient(this)
 
@@ -70,9 +80,35 @@ class ComposeActivity : AppCompatActivity() {
                 })
             }
         }
+
+        // Handles the text being changed in the EditText
+        etCompose.addTextChangedListener(object : TextWatcher {
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.i(TAG, "In beforeTextChanged().")
+                Log.i(TAG, "   count: $count")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.i(TAG, "In onTextChanged().")
+                Log.i(TAG, "   count: $count")
+                charactersRemaining = CHARACTER_LIMIT - count
+                tvCharacterCount.text = charactersRemaining.toString()
+                if (charactersRemaining < 0) {
+                    tvCharacterCount.setTextColor(Color.RED)
+                } else {
+                    tvCharacterCount.setTextColor(Color.GRAY)
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                Log.i(TAG, "In afterTextChanged().")
+            }
+        })
     }
 
     companion object {
         val TAG = "ComposeActivity"
+        const val CHARACTER_LIMIT = 280
     }
 }
